4eb8d8014313263f96798710a0b6903fd7b73f93
brunosimm <bruno.simm@edu.pucrs.br>
2021-05-07 13:24:30 -0300

    Routes

---
+++ frontend/src/components/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div>
+                <h1>pagina home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
