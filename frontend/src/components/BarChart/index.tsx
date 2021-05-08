114cd4caf6c11a4c6347ff7d2ef7db43439a21d6
brunosimm <bruno.simm@edu.pucrs.br>
2021-05-08 18:19:26 -0300

    BarChart Integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,60 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSucess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[]
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>(
+        {
+            labels: {
+                categories: []
+            },
+            series: [
+                {
+                    name: "",
+                    data: []                   
+                }
+            ]
+        }
+    );
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSucess[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round((x.deals/x.visited) * 100, 1));
+
+                setChartData(
+                    {
+                        labels: {
+                            categories: myLabels
+                        },
+                        series: [
+                            {
+                                name: "% Sucess",
+                                data: mySeries                  
+                            }
+                        ]
+                    }
+                );
+            });
+    }, []);
 
     const options = {
         plotOptions: {
@@ -10,6 +64,7 @@ const BarChart = () => {
         },
     };
     
+    /*
     const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -21,11 +76,12 @@ const BarChart = () => {
             }
         ]
     };
+    */
 
     return (
         <Chart 
-            options = {{...options,xaxis: mockData.labels}}
-            series = {mockData.series}
+            options = {{...options,xaxis: chartData.labels}}
+            series = {chartData.series}
             type = "bar"
             height = "240"
         />
