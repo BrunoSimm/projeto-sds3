4fa41589c17ab0097e63f8a8003a1994e12ab556
brunosimm <bruno.simm@edu.pucrs.br>
2021-05-08 19:32:29 -0300

    Datatable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,9 +1,37 @@
-export type SaleSum = {
+import { Seller } from "./seller";
+
+export type Sale = 
+{
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    first: boolean;
+    numberOfElements?: number;
+    size?: number;
+    number: number;
+    empty?: boolean;
+};
+
+
+
+export type SaleSum = 
+{
     sellerName: string;
     sum: number;
 };
 
-export type SaleSucess = {
+export type SaleSucess = 
+{
     sellerName: string;
     visited: number;
     deals: number;
